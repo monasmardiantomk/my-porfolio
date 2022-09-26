369a8751c9ad0f8cc9ca6cb1de3dd20ba4235ba4
monasmardiantomk <monasmardiantomk02@gmail.com>
2022-09-26 22:09:09 +0700

    full porfolio

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+
+  theme: {
+    container: {
+      center: true,
+      padding: "30px",
+    },
+    extend: {
+      colors: {
+        primary: "#22d3ee",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
